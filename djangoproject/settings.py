5f277f3ad416a531eaeb58285d5c00325375f35b
gokul-vscode <ramgokul637@gmail.com>
2026-01-06 16:43:43 +0530

    Fix render database ssl and settings

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-$05$q)8p)rt&0=5n+-@hb+#!ob1d(v2d6(j9pb0%ekz-#@vf8-'
-import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-unsafe")
 
@@ -81,14 +80,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-import os
 
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 }
 
