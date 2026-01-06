462072367ee43a3bd99870222ffb44afd5ef6fa5
gokul-vscode <ramgokul637@gmail.com>
2026-01-06 17:22:47 +0530

    settings.py updated

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangoapp',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,6 +75,7 @@ TEMPLATES = [
         },
     },
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
