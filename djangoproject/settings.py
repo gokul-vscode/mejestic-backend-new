abc80bf218135a8e41e1b2f1f3ea704f813de8f4
gokul-vscode <ramgokul637@gmail.com>
2026-01-06 15:20:45 +0530

    error process

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-$05$q)8p)rt&0=5n+-@hb+#!ob1d(v2d6(j9pb0%ekz-#@vf8-'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-unsafe")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -79,11 +81,17 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False
     )
 }
+
 
 
 # Password validation
